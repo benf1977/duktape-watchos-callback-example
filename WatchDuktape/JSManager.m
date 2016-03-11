fb70ee7d55c6d478723bf88b93cac2dfaa0ffdbc
Benjamin Flynn <benjamin.flynn@bflysoftware.com>
2016-03-11 11:25:59 -0800

    Still seeing the callback even with operation queues.

--- WatchDuktape/JSManager.m
+++ WatchDuktape/JSManager.m
@@ -12,6 +12,7 @@
 
 @interface JSManager ()
 @property (nonatomic, strong) NSValue               *context;
+@property (nonatomic, strong) NSOperationQueue      *operationQueue;
 @end
 
 
@@ -36,20 +37,33 @@ duk_ret_t native_callOut(duk_context *ctx)
     return _sharedInstance;
 }
 
+- (instancetype)init
+{
+    self = [super init];
+    if (self)
+    {
+        self.operationQueue = [[NSOperationQueue alloc] init];
+        self.operationQueue.maxConcurrentOperationCount = 1;
+    }
+    return self;
+}
+
 - (void)go
 {
     NSString *path = [[NSBundle mainBundle] pathForResource:@"example" ofType:@"js"];
     NSLog(@"Load file at path: %@", path);
     
-    duk_context *ctx = NULL;
-    ctx = duk_create_heap_default();
-    duk_push_global_object(ctx);
-    
-    duk_push_global_object(ctx);
-    duk_push_c_function(ctx, native_callOut, 1);
-    duk_put_prop_string(ctx, -2, "native_callOut");
-    
-    self.context = [NSValue valueWithPointer:ctx];
+    [self.operationQueue addOperationWithBlock:^{
+        duk_context *ctx = NULL;
+        ctx = duk_create_heap_default();
+        duk_push_global_object(ctx);
+        
+        duk_push_global_object(ctx);
+        duk_push_c_function(ctx, native_callOut, 1);
+        duk_put_prop_string(ctx, -2, "native_callOut");
+        
+        self.context = [NSValue valueWithPointer:ctx];
+    }];
     
     [self processJSFile:path completion:^(BOOL success) {
         NSLog(@"Done (%@)", @(success));
@@ -71,44 +85,48 @@ duk_ret_t native_callOut(duk_context *ctx)
 
 - (void)processJSCommand:(NSString *)command resultBlock:(void (^)(NSString *stringifiedResult))resultBlock
 {
-    NSString *wrappedExpression = [NSString stringWithFormat:@"JSON.stringify(%@);", command];
-    NSLog(@"Command: %@", wrappedExpression);
-    duk_context *ctx = self.context.pointerValue;
-    NSDate* date = [NSDate date];
-    NSLog(@"PRIOR POP CONTEXT: %@", [wrappedExpression substringToIndex:MIN(48, wrappedExpression.length)]);
-    duk_peval_string(ctx, [wrappedExpression cStringUsingEncoding:NSUTF8StringEncoding]);
-    const char * output = duk_safe_to_string(ctx, -1);
-    
-    NSString *stringifiedResult = nil;
-    if (output)
-    {
-        stringifiedResult = [NSString stringWithUTF8String:output];
-    }
-    
-    duk_pop(ctx);
-    
-    NSLog(@"POST POP CONTEXT: %@ %f", [wrappedExpression substringToIndex:MIN(48, wrappedExpression.length)], ABS(([date timeIntervalSinceDate:[NSDate date]])));
-    [self handleResultBlock:resultBlock value:stringifiedResult];
+    [self.operationQueue addOperationWithBlock:^{
+        NSString *wrappedExpression = [NSString stringWithFormat:@"JSON.stringify(%@);", command];
+        NSLog(@"Command: %@", wrappedExpression);
+        duk_context *ctx = self.context.pointerValue;
+        NSDate* date = [NSDate date];
+        NSLog(@"PRIOR POP CONTEXT: %@", [wrappedExpression substringToIndex:MIN(48, wrappedExpression.length)]);
+        duk_peval_string(ctx, [wrappedExpression cStringUsingEncoding:NSUTF8StringEncoding]);
+        const char * output = duk_safe_to_string(ctx, -1);
+        
+        NSString *stringifiedResult = nil;
+        if (output)
+        {
+            stringifiedResult = [NSString stringWithUTF8String:output];
+        }
+        
+        duk_pop(ctx);
+        
+        NSLog(@"POST POP CONTEXT: %@ %f", [wrappedExpression substringToIndex:MIN(48, wrappedExpression.length)], ABS(([date timeIntervalSinceDate:[NSDate date]])));
+        [self handleResultBlock:resultBlock value:stringifiedResult];
+    }];
 }
 
 - (void)processJSFile:(NSString *)path completion:(void (^)(BOOL))completion
 {
-    NSLog(@"Process file: %@", path);
-    duk_context *ctx = self.context.pointerValue;
-    
-    int failed = duk_peval_file(ctx, [path cStringUsingEncoding:NSUTF8StringEncoding]);
-    if (failed)
-    {
-        NSLog(@"Failed to evalaute file %@: %s", path, duk_safe_to_string(ctx, -1));
-        duk_pop(ctx);
-        [self handleCompletion:completion success:NO];
-    }
-    else
-    {
-        NSLog(@"Success.");
-        duk_pop(ctx);
-        [self handleCompletion:completion success:YES];
-    }
+    [self.operationQueue addOperationWithBlock:^{
+        NSLog(@"Process file: %@", path);
+        duk_context *ctx = self.context.pointerValue;
+        
+        int failed = duk_peval_file(ctx, [path cStringUsingEncoding:NSUTF8StringEncoding]);
+        if (failed)
+        {
+            NSLog(@"Failed to evalaute file %@: %s", path, duk_safe_to_string(ctx, -1));
+            duk_pop(ctx);
+            [self handleCompletion:completion success:NO];
+        }
+        else
+        {
+            NSLog(@"Success.");
+            duk_pop(ctx);
+            [self handleCompletion:completion success:YES];
+        }
+    }];
 }
 
 - (void)handleCompletion:(void (^)(BOOL success))completion success:(BOOL)success
